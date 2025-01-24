2de5212e4c17afad95213992cb6788cbf2c8ea29
Rodrigo de Moraes Santa Cruz <rodrigomoraessantacruz@yahoo.com.br>
2025-01-24 15:35:25 -0300

    2025-01-23 - Cap5, Cap6

--- Cap6-Logging/03_customlog_tarefa.py
+++ Cap6-Logging/03_customlog_tarefa.py
@@ -1,15 +1,23 @@
 # Personalizando o retorno do logging
 import logging
 
+dados = {"pessoa": "jess"}
+
 
 # TODO: Crie uma outra função para rodar o log
-
+def outra_funcao():
+    logging.info("Esta é uma mensagem de informação")
 
 def main():
     # TODO: Defina o nível de log para debug e um arquivo para salvar
     # o retorno. Use também uma formatação personalizada
-    logging.info("Esta é uma mensagem de informação", extra=dados)
-    logging.warning("Esta é uma mensagem de aviso", extra=dados)
+    formato = "%(asctime)s %(levelname)s %(funcName)s Pessoa:%(pessoa)s"
+    logging.basicConfig(filename="output.log", level=logging.DEBUG, format = formato)
+
+    logging.info("Esta é uma mensagem de informação", extra = dados)
+    logging.warning("Esta é uma mensagem de aviso", extra = dados)
+
+    outra_funcao()
 
 
 if __name__ == "__main__":
